bd045faba0b72ae442837e8e6d63bf5558d8cd01
sairakesh118 <sairakesh150@gmail.com>
2025-06-28 22:58:00 +0530

    changes

--- main.py
+++ main.py
@@ -26,13 +26,16 @@ class EmailRequest(BaseModel):
     message: str
 
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://finance-2rq1.vercel.app/"],
+    allow_origins=["https://finance-2rq1.vercel.app", "http://localhost:3000"],  # no trailing slash
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.post("/send-email")
 async def trigger_email(data: EmailRequest, background_tasks: BackgroundTasks):
